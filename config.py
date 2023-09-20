aaee3ed88a541f51db812da70c7c91954ae339db
simone-costamagna <simone.costamagna99@gmail.com>
2023-09-20 15:18:27 +0200

    Test sending an email

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Email address to receive notifications
 EMAIL_ADDRESS = "scostamagna.momo@gmail.com"
 
-# Dictionary of products to watch. The key is the product link, while the value is the maximum price
+# Dictionary of products to watch. The key is the product link, while the value is the established price
 PRODUCTS = {
-    "https://www.amazon.it/": 200,
+    "https://www.amazon.it/": 200.00,
 }
--- main.py
+++ main.py
@@ -4,9 +4,48 @@ import smtplib
 from email.mime.text import MIMEText
 
 
+class GetDriverException(Exception):
+    pass
+
+
+def get_driver() -> webdriver.chrome.webdriver.WebDriver:
+    """
+    get the web browsing driver.
+
+    :return chrome_driver: driver for web browsing
+    """
+
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_driver = webdriver.Chrome(options=chrome_options)
+
+    except Exception as ex:
+        raise GetDriverException('Cannot get the driver from Chrome.', str(ex))
+
+    return chrome_driver
+
+
+def check_price(driver: webdriver.chrome.webdriver.WebDriver, link: str, established_price: float) -> str:
+    """
+    Verify the cost of an item and determine whether it falls below the established price.
+
+    :param driver: driver for web browsing
+    :param link: product link
+    :param established_price: established price
+    :return: content of the email with the price information if the outcome is positive otherwise None
+    """
+
+    driver.get(str)
+
+    return driver.page_source
+
+
 def send_email(address: str, subject: str, content: str):
     """
-    An email is generated and sent when provided with an email address, subject, and content
+    An email is generated and sent when provided with an email address, subject, and content.
 
     :param address: recipient's email address
     :param subject: subject of the email
@@ -32,3 +71,10 @@ def send_email(address: str, subject: str, content: str):
 
 if __name__ == "__main__":
     send_email(EMAIL_ADDRESS, "Update from check-price-amazon", "Testing")
+
+    driver = get_driver()
+    for product_link, product_price in PRODUCTS.items():
+        response = check_price(driver, product_link, product_price)
+
+        if response is not None:
+            send_email(EMAIL_ADDRESS, "Update from check-price-amazon", response)
