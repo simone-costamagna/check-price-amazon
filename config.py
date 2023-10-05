ee895c72bc2ddeeebe7c22c4a6fc90a1ee19ca21
Costamagna-Simone <73221274+simone-costamagna@users.noreply.github.com>
2023-10-05 10:19:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ EMAIL_ADDRESS = "scostamagna.momo@gmail.com"
 
 # Dictionary of products to watch. The key is the product link, while the value is the established price
 PRODUCTS = {
-    "https://www.amazon.it/Marshall-Cuffia-Major-Bluetooth-Nero/dp/B08KHT2HDT?th=1": 115.00,
+    "https://www.amazon.it/Marshall-Cuffia-Major-Bluetooth-Nero/dp/B08KHT2HDT?th=1": 120.00,
     "https://www.amazon.it/JBL-Pieghevole-Connessione-Multipoint-Assistente/dp/B08VD6SRBZ?th=1": 30.00,
 }
